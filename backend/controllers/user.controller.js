3f53d478306c5d9ab88e93cc7e603e36f4f8cee5
harshanj07 <jharshan07@gmail.com>
2025-01-23 23:46:40 +0530

    fixing

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -9,6 +9,6 @@ export const getUsersForSidebar = async (req, res) => {
 		res.status(200).json(filteredUsers);
 	} catch (error) {
 		console.error("Error in getUsersForSidebar: ", error.message);
-		res.status(500).json({ error: "Internal server error" });
+		res.status(500).json({ error: "Internal server errordfbdfhdjk" });
 	}
 };
